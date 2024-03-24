4366e439824ddaa0cbe2a30bc437cedc1a7b6928
Mitchell Jo <mitchjo898@gmail.com>
2024-03-24 21:58:21 +1100

    updated file to generate text using the user's input

--- load_ai_model.py
+++ load_ai_model.py
@@ -18,21 +18,6 @@ if __name__ == '__main__':
     def tokenize_function(examples):
         return tokenizer(examples['card info'])
 
-    def group_texts(examples):
-        # Concatenate all texts.
-        concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-        total_length = len(concatenated_examples[list(examples.keys())[0]])
-        # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-            # customize this part to your needs.
-        total_length = (total_length // block_size) * block_size
-        # Split by chunks of max_len.
-        result = {
-            k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
-            for k, t in concatenated_examples.items()
-        }
-        result["labels"] = result["input_ids"].copy()
-        return result
-
     from transformers import AutoModelForCausalLM
     model = AutoModelForCausalLM.from_pretrained("ai_model/.")
 
@@ -41,11 +26,14 @@ if __name__ == '__main__':
 
         from transformers import pipeline
         generator = pipeline('text-generation', model = model, tokenizer = tokenizer, device="cuda")
-        output = generator(text_string, max_length = 300, num_return_sequences=3)
+        output = generator(text_string, max_length = 300, num_return_sequences=5)
 
         for output_sequence in output:
             output_string = output_sequence['generated_text']
             print(output_string.split('...')[0])
 
-    print(generate_text("test 1"))
-    print(generate_text("Infernoble Knight "))
+    #generate_text("test 1")
+    #generate_text("Infernoble Knight ")
+
+    input_string = input("Enter text: ")
+    generate_text(input_string)
